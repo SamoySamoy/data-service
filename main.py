3af99b76b1914b267769a24a57f89a785d9dee29
samoy <antalois23@gmail.com>
2023-05-25 11:29:08 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.config import settings
+from app.routers import auth, user, post
+
+app = FastAPI()
+
+origins = [
+    settings.CLIENT_ORIGIN,
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api/checker")
+def root():
+    return {"message": "Check success"}
