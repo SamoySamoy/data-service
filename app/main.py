ad6b1a6f6df58864acbf6f15697ea9f6bc68f339
samoy <antalois23@gmail.com>
2023-05-25 11:47:45 +0700

    ops: env config

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.config import settings
-from app.routers import auth, user, post
+from app.routers import auth, user, post, holiday
 
 app = FastAPI()
 
