9b72d6ed1a32ab10fa8ec4d4fadde082ff5d8cf5
samoy <antalois23@gmail.com>
2023-05-29 10:20:23 +0700

    feat: add fetch stock price

--- app/crawl.py
+++ app/crawl.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
 
 url = "https://finfo-iboard.ssi.com.vn/graphql"
 
@@ -27,6 +28,7 @@ def get_stock_company_profile(stock: str):
     return soup.get_text()
 
 
+# fetch stock's shareholders (50 biggest)
 def get_stock_shareholders(stock: str):
     payload = json.dumps(
         {
@@ -45,4 +47,36 @@ def get_stock_shareholders(stock: str):
     response = requests.request("POST", url, headers=headers, data=payload).json()
     results = response["data"]["shareholders"]["dataList"]
     return results
+
+
+# fetch stock prices (50 latest)
+def get_stock_prices(stock: str):
+    current_date = datetime.now().strftime("%d/%m/%Y")
+    start_date = (datetime.now() - timedelta(days=50)).strftime("%d/%m/%Y")
+
+    payload = json.dumps(
+        {
+            "operationName": "stockPrice",
+            "variables": {
+                "symbol": "BCM",
+                "offset": 1,
+                "size": 10,
+                "fromDate": start_date,
+                "toDate": current_date,
+            },
+            "query": "query stockPrice($symbol: String!, $size: Int, $offset: Int, $fromDate: String, $toDate: String) {\n  stockPrice(\n    symbol: $symbol\n    size: $size\n    offset: $offset\n    fromDate: $fromDate\n    toDate: $toDate\n  )\n}\n",
+        }
+    )
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36",
+        "Content-Type": "application/json",
+        "Cookie": "__cf_bm=.uP_FP0tcppzCjuQCIf_yncTYWQjVYye6rzL_C2FX.8-1685323698-0-AZ9xW26q88hLUZ7LOowxT3poSha2ekiNctcIZH8fXO7p1j6fqF7tqo4YdKa8dYFKQATZxjg9U0uMIjp3LOlR9ko=",
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload).json()
+    results = response["data"]["stockPrice"]["dataList"]
+    return results
+
+
 
